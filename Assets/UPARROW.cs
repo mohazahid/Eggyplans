060a0ebffd051a1a2c43dbf4cd1acd766e91c344
mohazahid <adyanzahid786@gmail.com>
2022-10-08 22:34:07 -0700

    everything

---
+++ Assets/UPARROW.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UPARROW : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.SetCursor(cursorTexture, Vector2.zero, CursorMode.Auto);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
